9cf019a0acab6c3ead905460fd406a6b0c8d0d37
proffatai <proffatai@gmail.com>
2022-08-02 07:36:20 +0100

    Learning more

--- cypress/integration/1st_project.js
+++ cypress/integration/1st_project.js
@@ -3,8 +3,10 @@
  it ("Sample file", function()
  {
     cy.visit("https://www.google.com") // cy.visit() to open any website
-    cy.get('.gLFyf').type("HP laptop").click()
+    cy.get('.gLFyf').type("HP laptop {enter}")
     //cy.get('.gLFyf').type("Hello world Doggy.com {enter}") //cy.get() is to access or locate any object on the page
+
+    cy.get('.g > :nth-child(1) > [lang="en"] > .GLI8Bc > .jGGQ5e > .yuRUbf > a > .LC20lb').click()
  }) // creating an it () mocha test runner
 
  //cy.get('.gLFyf').type("Hello world Doggy.com"), we just located the object with class name .gLFyf and typed some texts into it
