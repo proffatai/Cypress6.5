b48504efbb4f7567020da145d62f95c8d36272a4
proffatai <proffatai@gmail.com>
2022-08-02 17:08:38 +0100

    Using cy.contains('textName') to access an element via its text and also using cy.wait(time) to add some delays before moving to next action

--- cypress/integration/1st_project.js
+++ cypress/integration/1st_project.js
@@ -2,11 +2,14 @@
 
  it ("Sample file", function()
  {
-    cy.visit("https://www.google.com") // cy.visit() to open any website
-    cy.get('.gLFyf').type("HP laptop {enter}")
-    //cy.get('.gLFyf').type("Hello world Doggy.com {enter}") //cy.get() is to access or locate any object on the page
-
-    cy.get('.g > :nth-child(1) > [lang="en"] > .GLI8Bc > .jGGQ5e > .yuRUbf > a > .LC20lb').click()
+    cy.visit("https://www.google.com"); // cy.visit() to open any website
+    cy.get('.gLFyf').type("HP laptop {enter}");
+    //cy.get('.gLFyf').type("Hello world Doggy.com {enter}") ;//cy.get() is to access or locate any object on the page
+   cy.contains('Videos').click() ; 
+   cy.wait(3000) // we wanna wait for 3s before clicking the element that has longest
+      cy.contains('longest').click() ;
+   // cy.get('.g > :nth-child(1) > [lang="en"] > .GLI8Bc > .jGGQ5e > .yuRUbf > a > .LC20lb').click();
+   // cy.get('.right > :nth-child(3) > a').click();
  }) // creating an it () mocha test runner
 
  //cy.get('.gLFyf').type("Hello world Doggy.com"), we just located the object with class name .gLFyf and typed some texts into it
