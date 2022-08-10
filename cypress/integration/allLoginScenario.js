10ce2f24bcdd14855eb2adac975dd3251e53c404
proffatai <proffatai@gmail.com>
2022-08-10 13:25:16 +0100

    corrected login username

--- cypress/integration/allLoginScenario.js
+++ cypress/integration/allLoginScenario.js
@@ -14,7 +14,7 @@
  it ("Valid Login", function()
  {
     cy.visit('https://opensource-demo.orangehrmlive.com/index.php/auth/login');
-    cy.get('#txtUsername').type('admin')
+    cy.get('#txtUsername').type('Admin')
     cy.get('#txtPassword').type('admin123')
     cy.get('#btnLogin').click()
  })
